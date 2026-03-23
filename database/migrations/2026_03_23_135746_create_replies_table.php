bb0c27988a2b0d93449a3dc62ed5287705ba2d41
Анастасия <anastasis.dnn@mail.ru>
2026-03-23 17:03:27 +0300

    Инициирован проект, добавлены модели, контроллеры

---
+++ database/migrations/2026_03_23_135746_create_replies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('message');
+            $table->string('email_reply');
+            $table->foreignIdFor(\App\Models\Message::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('replies');
+    }
+};
