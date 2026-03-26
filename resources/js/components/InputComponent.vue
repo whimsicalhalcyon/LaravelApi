1c2d4845f6d9367a9b016e4fe4faa9c12f2591e2
Анастасия <anastasis.dnn@mail.ru>
2026-03-26 13:20:58 +0300

    добавлены миграции

---
+++ resources/js/components/InputComponent.vue
@@ -0,0 +1,33 @@
+<script>
+import {St} from "vite/dist/node/chunks/node.js";
+
+export default {
+    props: {
+        type: {
+            type: String
+        },
+
+        name: {
+            type: String,
+        },
+
+        placeholder: {
+            type: String,
+            required: false
+        },
+
+        id: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
+
+<template>
+    <input :type="type" :name="name" :placeholder="placeholder" class="border border-slate-100 p-2 rounded-md w-1/2">
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SlideComponent.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+        props : {
+            open_slide: Boolean,
+            default: false
+        },
+
+        methods: {
+
+        }
+    }
+</script>
+
+<template>
+    <div class="slide p-4">
+        <p>Ответ на сообщение</p>
+        <div class="head-email">
+            <div class="send-email flex w-full flex-col gap-4">
+                <input type="text" placeholder="От кого" class="border border-slate-100 rounded-md p-3 w-full">
+                <input type="text" placeholder="Кому" class="border border-slate-100 rounded-md p-3 w-full">
+            </div>
+        </div>
+        <div class="body-email mt-12 ">
+            <label class="mb-3" for="message">Введите текст сообщения</label>
+            <textarea class="w-full border border-slate-100 rounded-md p-3" name="message">Тест</textarea>
+        </div>
+        <button class="p-2 text-white bg-slate-400 rounded-md cursor-pointer">Отправить</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
