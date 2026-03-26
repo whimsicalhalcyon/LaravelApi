99bf6f63c93da5cfe18e2f6cf25f90d94754ab26
Анастасия <anastasis.dnn@mail.ru>
2026-03-26 21:24:03 +0300

    исправлен вывод сообщений

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,20 @@
 <script>
 import LinkComponent from "./components/LinkComponent.vue";
 import MenuPanel from "./components/MenuPanel.vue";
+import SlideComponent from "@/components/SlideComponent.vue";
+import InputComponent from "@/components/InputComponent.vue";
 export default {
     components: {
+        InputComponent,
+        SlideComponent,
         MenuPanel,
-        LinkComponent
+        LinkComponent,
     },
 
     data() {
         return {
             messages: [],
+            openSlideMessage: false
         }
     },
     methods: {
@@ -22,6 +27,10 @@ export default {
                 console.error(error)
                 alert('Не удалось загрузить данные')
             }
+        },
+        openSlide() {
+            this.openSlideMessage = !this.openSlideMessage
+            console.log(this.openSlideMessage)
         }
     },
     mounted() {
@@ -33,8 +42,29 @@ export default {
 <template>
     <div class="container flex">
         <menu-panel open-panel="true"></menu-panel>
-        <ul>
-            <li v-for="msg in messages" :key="msg.id" class="border border-slate-600 p-2 rounded-sm mt-3 w-full">title: {{msg.title}} message: {{msg.message}}</li>
-        </ul>
+        <div class="main w-full p-5">
+            <div class="navbar mt-5 mb-3 w-full">
+                <input-component type="text" placeholder="Поиск" name="search" />
+                <input type="text" placeholder="Поиск" class="border border-slate-100 p-2 rounded-md w-1/2">
+                <input type="date" placeholder="Дата" class="border border-slate-100 p-2 rounded-md ms-2">
+                <input type="date" placeholder="Дата" class="border border-slate-100 p-2 rounded-md ms-2">
+            </div>
+
+            <div class="messages flex">
+                <div class="card-message w-1/2">
+                    <div class="card border border-slate-100 p-3 p-2 flex justify-between" v-for="message in messages" :key="message.id">
+                        <div class="message-user">
+                            <a class="title font-bold text-xl decoration-0 cursor-pointer" @click="openSlide">{{message.title}}</a>
+                            <p class="email_send text-slate-600">{{message.email_send}}</p>
+                            <p class="message">{{message.message}}</p>
+                        </div>
+                        <p>{{message.created_at}}</p>
+                    </div>
+                </div>
+
+                <slide-component :open_slide="openSlideMessage" class="w-1/2"></slide-component>
+            </div>
+
+        </div>
     </div>
 </template>
